f2f877097b30d3b4494f6de03405390c04ab5589
amirhesham65 <amirhesham65@gmail.com>
2020-03-12 15:40:54 +0200

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Amir Hesham on 3/12/20.
+//  Copyright © 2020 Amir Hesham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
